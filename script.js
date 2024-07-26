b380f57ae4ce74deb789b2ed1572758acf6cc543
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-07-25 21:55:58 -0400

    javascript step2-stp 6

--- script.js
+++ script.js
@@ -1,1 +1,30 @@
-console.log("Hello World")
+
+// a funtion to output random rock paper or scissors
+// function getComputerChoice()
+//    list <- [rock, paper, scissors]
+//    i <- Math.floor(Math.random() * 4)
+//    return list[i]
+
+
+
+
+function getComputerChoice() {
+    let game = ["rock", "paper", "scissors"];
+    i = Math.floor(Math.random() * 3);
+    console.log(game[i]);
+    return list[i];
+}
+
+
+// logic that gets human choices
+// function getHumanChoice
+// let input <- prompt(Rock, Paper or Scissors)
+// return input
+
+
+function getHumanChoice() {
+    let input = prompt("Rock, Paper, Scissors")
+    console.log(input)
+}
+
+getHumanChoice();
