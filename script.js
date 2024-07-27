21414c3ac73b0ebba011a12abb9d479db7a308a7
samueljai120 <173202232+samueljai120@users.noreply.github.com>
2024-07-27 17:31:43 -0400

    rearranged all indentation for proper structure

--- script.js
+++ script.js
@@ -10,76 +10,76 @@ document.getElementById("button").addEventListener("click", () => {
     playGame(5); // The game will have
 })
     
-    function playGame(max){
+function playGame(max){
     // This is to loop the game according to the max value
     while (humanScore <= max && computerScore <= max) {
         // The below three console will print a nice layout for the Round
         console.log("")
         console.log("Round: " + gameRound++);
         console.log("")
-    // The function is to generate computer's choice
-    function getComputerChoice(){
+        // The function is to generate computer's choice
+        function getComputerChoice(){
         const choice = ["rock", "paper", "scissors"];
         const i = Math.floor(Math.random() * 3); // This is to generate random index 
         const comChoice = choice[i]; // This is to choose the value of the array according to the generated random index
         console.log("Computer's choice: " + comChoice);
         return comChoice;
-    }
+        }
     
-    // This function to retrieve input and print prompt
-    function getHumanChoice(){
+        // This function to retrieve input and print prompt
+        function getHumanChoice(){
         const choice = prompt("Enter Your Choice [ Rock, Paper or Scissors ]");
         const humChoice = choice.toLowerCase();
         console.log("Your choice: " + humChoice);
         return humChoice;
-    }
+        }
     
-    // Functions stored in variables
-    const humanChoice = getHumanChoice();
-    const computerChoice = getComputerChoice();
+        // Functions stored in variables
+        const humanChoice = getHumanChoice();
+        const computerChoice = getComputerChoice();
     
-    // If statement comparing human and computer choices
-    if(((humanChoice === "rock") && (computerChoice === "scissors")) 
+        // If statement comparing human and computer choices
+        if(((humanChoice === "rock") && (computerChoice === "scissors")) 
         || ((humanChoice === "paper") && (computerChoice === "rock")) 
-    || ((humanChoice === "scissors") && (computerChoice === "paper"))) {
-        humanScore += 1;
-        console.log("Human:" + humanScore + " Computer: " + computerScore);    
+        || ((humanChoice === "scissors") && (computerChoice === "paper"))) {
+            humanScore += 1;
+            console.log("Human:" + humanScore + " Computer: " + computerScore);    
         // humanWin = true;
             // computerWin = false;    
-        console.log("You Win!");
+            console.log("You Win!");
             // return "You Win!";
             
-    } else if(((humanChoice === "rock") && (computerChoice === "paper")) 
+        } else if(((humanChoice === "rock") && (computerChoice === "paper")) 
         || ((humanChoice === "paper") && (computerChoice === "scissors")) 
         || ((humanChoice === "scissors") && (computerChoice === "rock"))) {
-        computerScore += 1;
-        console.log("Human:" + humanScore + " Computer: " + computerScore);
+            computerScore += 1;
+            console.log("Human:" + humanScore + " Computer: " + computerScore);
         // computerWin = true;
         // humanWin = false;   
-        console.log("You lose!");
+            console.log("You lose!");
             // return "You lose!";
-    } else if (humanChoice === computerChoice) {
-        console.log("Human:" + humanScore + " Computer: " + computerScore); 
-        console.log("Tie!");
+        } else if (humanChoice === computerChoice) {
+            console.log("Human:" + humanScore + " Computer: " + computerScore); 
+            console.log("Tie!");
             // return "Tie!";
-    } else {
-        console.log("Human:" + humanScore + " Computer: " + computerScore);
-        console.log("Wrong Choice!");
+        } else {
+            console.log("Human:" + humanScore + " Computer: " + computerScore);
+            console.log("Wrong Choice!");
             // return "Wrong Choice!";
-    }
+        }
     
 
     // If statement to end the game
-    if (humanScore >= max || computerScore >= max) {
-        console.log("Game Over!");
-        if (humanScore = rounds) {
-            console.log("Congadulations! You are the WINNER!");
-        } else {
-            console.log("Good Game! Better luck next time!");
-        }
-        break;
+        if (humanScore >= max || computerScore >= max) {
+            console.log("Game Over!");
+            if (humanScore = rounds) {
+                console.log("Congadulations! You are the WINNER!");
+            } else {
+                console.log("Good Game! Better luck next time!");
+            }
+            break;
         }
         
     }
 
-    }
+}
